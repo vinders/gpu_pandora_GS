d8e4c70743e5a371e01a6406059382349b1df717
vinders <vinders@users.noreply.github.com>
2021-07-27 18:21:43 +0200

    feat-display: font-map manager + character descriptor

---
+++ display/include/display/font_map.h
@@ -0,0 +1,82 @@
+/*******************************************************************************
+Pandora GS - PSEmu-compatible GPU driver
+Copyright (C) 2021  Romain Vinders
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details (LICENSE file).
+*******************************************************************************/
+#pragma once
+
+#include <cstdint>
+#include <unordered_map>
+#include <system/align.h>
+
+namespace display {
+  /// @brief Font-map character descriptor (used with font-map spritesheet)
+  __align_prefix(16)
+  struct CharDescriptor final {
+    inline uint32_t id() const noexcept { return ((uint32_t*)data)[0]; } ///< Character code point (unicode)
+    inline uint8_t  x() const noexcept { return data[8]; }               ///< Horizontal image location in font-map spritesheet
+    inline uint32_t y() const noexcept { return ((uint32_t*)data)[1]; }  ///< Vertical image location in font-map spritesheet
+    inline uint8_t width() const noexcept { return data[10]; }           ///< Character image width
+    inline uint8_t height() const noexcept { return data[11]; }          ///< Character image height
+    inline int8_t  offsetX() const noexcept { return (int8_t)data[12]; } ///< Left padding before character (may be negative)
+    inline uint8_t offsetY() const noexcept { return data[13]; }         ///< Top padding before character
+    inline uint8_t advanceX() const noexcept { return data[14]; }        ///< Next caret position after 'x'
+    inline uint8_t advanceY() const noexcept { return data[15]; }        ///< Next line position after 'y' (== line_height - offsetY)
+      
+    inline void id(uint32_t charId) noexcept { ((uint32_t*)data)[0] = charId; } ///< Set character code point (unicode)
+    inline void x(uint8_t x) noexcept { data[8] = x; }                      ///< Set horizontal image location
+    inline void y(uint32_t y) noexcept { ((uint32_t*)data)[1] = y; }        ///< Set vertical image location
+    inline void width(uint8_t w) noexcept { data[10] = w; }                 ///< Set character image width
+    inline void height(uint8_t h) noexcept { data[11] = h; }                ///< Set character image height
+    inline void offsetX(int8_t padX) noexcept { data[12] = (uint8_t)padX; } ///< Set left padding before character
+    inline void offsetY(uint8_t padY) noexcept { data[13] = padY; }         ///< Set top padding before character
+    inline void advanceX(uint8_t advX) noexcept { data[14] = advX; }        ///< Set next caret position after 'x'
+    inline void advanceY(uint8_t advY) noexcept { data[15] = advY; }        ///< Set next line position after 'y'
+    uint8_t data[16];
+  } __align_suffix(16);
+
+  // ---
+
+  /// @brief Font-map spritesheet + character descriptors
+  template <typename _RenderApiTexture2D>
+  class FontMap final {
+  public:
+    FontMap() = default;
+    FontMap(const FontMap<_RenderApiTexture2D>&) = delete;
+    FontMap(FontMap<_RenderApiTexture2D>&&) noexcept = default;
+    FontMap& operator=(const FontMap<_RenderApiTexture2D>&) = delete;
+    FontMap& operator=(FontMap<_RenderApiTexture2D>&&) noexcept = default;
+    ~FontMap() noexcept {}
+
+    /// @brief Initialize font-map
+    /// @param spriteSheet      2D texture containing all character images
+    /// @param charDescriptors  Array of character location descriptors (must not be NULL)
+    /// @param length           Length of array 'charDescriptors'
+    FontMap(_RenderApiTexture2D&& spriteSheet, const CharDescriptor* charDescriptors, uint32_t length)
+      : _spriteSheet(spriteSheet) {
+      for (const CharDescriptor* it = charDescriptors; length; ++it, --length)
+        this->_descriptors[it->id()] = *it;
+    }
+
+    inline const _RenderApiTexture2D& spriteSheet() const noexcept { return this->_spriteSheet; } ///< Get texture to bind to renderer
+    inline size_t charCount() const noexcept { return _descriptors.size(); } ///< Number of character locations
+
+    /// @brief Find character descriptor by ID/char-code (returns NULL if not found)
+    inline const CharDescriptor* find(uint32_t charCode) const noexcept {
+      auto it = this->_descriptors.find(charCode);
+      return (it != this->_descriptors.end()) ? &(it->second) : nullptr;
+    }
+
+  private:
+    _RenderApiTexture2D _spriteSheet;
+    std::unordered_map<uint32_t,CharDescriptor> _descriptors;
+  };
+}
---
+++ display/test/font_map_test.cpp
@@ -0,0 +1,145 @@
+/*******************************************************************************
+Pandora GS - PSEmu-compatible GPU driver
+Copyright (C) 2021  Romain Vinders
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details (LICENSE file).
+*******************************************************************************/
+#include <gtest/gtest.h>
+#include <display/font_map.h>
+
+using namespace display;
+
+class FontMapTest : public testing::Test {
+public:
+protected:
+  //static void SetUpTestCase() {}
+  //static void TearDownTestCase() {}
+
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+
+TEST_F(FontMapTest, descriptorAccessors) {
+  CharDescriptor desc1;
+  desc1.id(125u);
+  desc1.x('\x10');
+  desc1.y(20u);
+  desc1.width('\x25');
+  desc1.height('\x35');
+  desc1.offsetX((int8_t)-5);
+  desc1.offsetY('\x5');
+  desc1.advanceX('\x22');
+  desc1.advanceY('\x40');
+
+  EXPECT_EQ((uint32_t)125u, desc1.id());
+  EXPECT_EQ((uint8_t)'\x10', desc1.x());
+  EXPECT_EQ((uint32_t)20u, desc1.y());
+  EXPECT_EQ((uint8_t)'\x25', desc1.width());
+  EXPECT_EQ((uint8_t)'\x35', desc1.height());
+  EXPECT_EQ((int8_t)-5, desc1.offsetX());
+  EXPECT_EQ((uint8_t)'\x5', desc1.offsetY());
+  EXPECT_EQ((uint8_t)'\x22', desc1.advanceX());
+  EXPECT_EQ((uint8_t)'\x40', desc1.advanceY());
+
+  CharDescriptor desc2 = desc1;
+  EXPECT_EQ((uint32_t)125u, desc2.id());
+  EXPECT_EQ((uint8_t)'\x10', desc2.x());
+  EXPECT_EQ((uint32_t)20u, desc2.y());
+  EXPECT_EQ((uint8_t)'\x25', desc2.width());
+  EXPECT_EQ((uint8_t)'\x35', desc2.height());
+  EXPECT_EQ((int8_t)-5, desc2.offsetX());
+  EXPECT_EQ((uint8_t)'\x5', desc2.offsetY());
+  EXPECT_EQ((uint8_t)'\x22', desc2.advanceX());
+  EXPECT_EQ((uint8_t)'\x40', desc2.advanceY());
+}
+
+struct FakeTexture final {
+  uint32_t handle = 0;
+};
+
+TEST_F(FontMapTest, createFontMap) {
+  FontMap<FakeTexture> defaultCtor;
+  EXPECT_EQ((uint32_t)0, defaultCtor.spriteSheet().handle);
+  EXPECT_EQ((size_t)0, defaultCtor.charCount());
+  EXPECT_TRUE(defaultCtor.find(0) == nullptr);
+
+  FontMap<FakeTexture> initCtor1(FakeTexture{ 1 }, nullptr, 0);
+  EXPECT_EQ((uint32_t)1, initCtor1.spriteSheet().handle);
+  EXPECT_EQ((size_t)0, initCtor1.charCount());
+  EXPECT_TRUE(initCtor1.find(0) == nullptr);
+
+  CharDescriptor descArray[3];
+  descArray[0].id(125u);
+  descArray[0].x('\x10');
+  descArray[0].y(20u);
+  descArray[0].width('\x25');
+  descArray[0].height('\x35');
+  descArray[0].offsetX((int8_t)-5);
+  descArray[0].offsetY('\x5');
+  descArray[0].advanceX('\x22');
+  descArray[0].advanceY('\x40');
+  descArray[1].id(32u);
+  descArray[1].x('\x12');
+  descArray[1].y(4u);
+  descArray[1].width('\x6');
+  descArray[1].height('\x8');
+  descArray[1].offsetX((int8_t)'\x5');
+  descArray[1].offsetY('\x20');
+  descArray[1].advanceX('\x4');
+  descArray[1].advanceY('\x6');
+  descArray[2].id(9999u);
+  descArray[2].x('\x0');
+  descArray[2].y(0);
+  descArray[2].width('\x20');
+  descArray[2].height('\x55');
+  descArray[2].offsetX((int8_t)0);
+  descArray[2].offsetY('\x0');
+  descArray[2].advanceX('\x25');
+  descArray[2].advanceY('\x60');
+
+  FontMap<FakeTexture> initCtor2(FakeTexture{ 2 }, descArray, 3);
+  EXPECT_EQ((uint32_t)2, initCtor2.spriteSheet().handle);
+  EXPECT_EQ((size_t)3, initCtor2.charCount());
+  EXPECT_TRUE(initCtor2.find(0) == nullptr);
+  ASSERT_FALSE(initCtor2.find(125) == nullptr);
+  auto* descPtr = initCtor2.find(125);
+  EXPECT_EQ((uint32_t)125u, descPtr->id());
+  EXPECT_EQ((uint8_t)'\x10', descPtr->x());
+  EXPECT_EQ((uint32_t)20u, descPtr->y());
+  EXPECT_EQ((uint8_t)'\x25', descPtr->width());
+  EXPECT_EQ((uint8_t)'\x35', descPtr->height());
+  EXPECT_EQ((int8_t)-5, descPtr->offsetX());
+  EXPECT_EQ((uint8_t)'\x5', descPtr->offsetY());
+  EXPECT_EQ((uint8_t)'\x22', descPtr->advanceX());
+  EXPECT_EQ((uint8_t)'\x40', descPtr->advanceY());
+  ASSERT_FALSE(initCtor2.find(32) == nullptr);
+  descPtr = initCtor2.find(32);
+  EXPECT_EQ((uint32_t)32u, descPtr->id());
+  EXPECT_EQ((uint8_t)'\x12', descPtr->x());
+  EXPECT_EQ((uint32_t)4u, descPtr->y());
+  EXPECT_EQ((uint8_t)'\x6', descPtr->width());
+  EXPECT_EQ((uint8_t)'\x8', descPtr->height());
+  EXPECT_EQ((int8_t)'\x5', descPtr->offsetX());
+  EXPECT_EQ((uint8_t)'\x20', descPtr->offsetY());
+  EXPECT_EQ((uint8_t)'\x4', descPtr->advanceX());
+  EXPECT_EQ((uint8_t)'\x6', descPtr->advanceY());
+  ASSERT_FALSE(initCtor2.find(9999) == nullptr);
+  descPtr = initCtor2.find(9999);
+  EXPECT_EQ((uint32_t)9999u, descPtr->id());
+  EXPECT_EQ((uint8_t)'\x0', descPtr->x());
+  EXPECT_EQ((uint32_t)0u, descPtr->y());
+  EXPECT_EQ((uint8_t)'\x20', descPtr->width());
+  EXPECT_EQ((uint8_t)'\x55', descPtr->height());
+  EXPECT_EQ((int8_t)0, descPtr->offsetX());
+  EXPECT_EQ((uint8_t)'\x0', descPtr->offsetY());
+  EXPECT_EQ((uint8_t)'\x25', descPtr->advanceX());
+  EXPECT_EQ((uint8_t)'\x60', descPtr->advanceY());
+}
