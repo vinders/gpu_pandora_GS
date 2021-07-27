274f094f782aeee291ac2c592fea96f589c6b36c
vinders <vinders@users.noreply.github.com>
2021-07-27 18:17:21 +0200

    infra-project: add license info

--- config/test/main.cpp
+++ config/test/main.cpp
@@ -1,9 +1,19 @@
 /*******************************************************************************
-Main execution of test cases
+Pandora GS - PSEmu-compatible GPU driver
+Copyright (C) 2021  Romain Vinders
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details (LICENSE file).
 *******************************************************************************/
 #include "gtest/gtest.h"
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
